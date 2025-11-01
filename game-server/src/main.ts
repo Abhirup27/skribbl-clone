29aa39981ea1a287ee15cd798716a8ca94c23a9e
Abhirup27 <55529237+Abhirup27@users.noreply.github.com>
2025-11-01 16:34:59 +0530

    Inital commit.

---
+++ game-server/src/main.ts
@@ -0,0 +1,16 @@
+// const { App } = require("uWebSockets.js");
+// const { Server } = require("socket.io");
+import { App } from "uWebSockets.js";
+import { Server } from "socket.io";
+const app = App();
+const io = new Server();
+
+io.attachApp(app);
+
+io.on("connection", (socket) => {});
+
+app.listen(3000, (token) => {
+  if (!token) {
+    console.warn("port already in use");
+  }
+});
